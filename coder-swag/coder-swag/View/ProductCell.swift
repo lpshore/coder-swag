56f7ad65ac4f3a48bb0e1e9121f6e6bb4ec543c8
Luke Shore <LukeShore@Lukes-MacBook-Air.local>
2018-04-05 21:55:03 +1000

    added collection views

---
+++ coder-swag/coder-swag/View/ProductCell.swift
@@ -0,0 +1,13 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Luke Shore on 5/4/18.
+//  Copyright © 2018 Luke Shore. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+}
